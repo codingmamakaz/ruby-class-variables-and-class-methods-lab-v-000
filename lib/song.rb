22292630baecf0944f9f77fb0f3c8253112ad619
Kazumi Karbowski <kazkazinus@gmail.com>
2018-04-19 02:36:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 class Song
   attr_accessor = :name, :artist, :genre
-
+  @@count = 0 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
@@ -18,5 +18,5 @@ class Song
   def genre
     @genre
   end
-  
+
 end
